898b5a22001dce35a4980c4df27fe6b9b416de88
Leventi <alexinps@gmail.com>
2022-02-23 12:45:13 +0300

    change get token func

--- fas_req.py
+++ fas_req.py
@@ -1,37 +1,51 @@
-import requests
-from lxml import etree
+from requests import Session
+from bs4 import BeautifulSoup
 
 
-test_url = 'https://webhook.site/3bf819a3-accf-4f1f-9c0a-aee6d724f3e9?'
+def get_token(text):
+    soup = BeautifulSoup(text, 'html.parser')
+    el = soup.find("input", {"name": "__RequestVerificationToken"})
+    return el['value']
 
+
+cookies_url = 'http://apps.eias.fas.gov.ru/FindCem/'
 url = 'http://apps.eias.fas.gov.ru/FindCEM/Search/Report'
-session = requests.session()
+
+session = Session()
+
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'Host': 'apps.eias.fas.gov.ru',
+    'Pragma': 'no-cache',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+}
 
 
 """Получаем cookie"""
-cookies_url = 'http://apps.eias.fas.gov.ru/FindCem/'
-
-get_cookies = session.get(url=cookies_url)
-cookie = get_cookies.cookies
-print(f'Cookie: {cookie}')
-
+get_cookies = session.get(url=cookies_url, headers=headers)
 
 """Получаем токен формы"""
-htmlparser = etree.HTMLParser()
-tree = etree.parse(cookies_url, htmlparser)
-
-token = tree.xpath('//input[@name="__RequestVerificationToken"]/@value')[0]
-print(f'Token: {token}')
-
-
+token = get_token(get_cookies.text)
 
 payload = {
-    '__RequestVerificationToken': token
+    '__RequestVerificationToken': token,
+    'RegTypeID': 0,
+    'RegPartID': 0,
+    'RegionID': 0,
+    'OrgName': '',
+    'INN': '',
+    'OKPO': '',
+    'OGRN': '',
 }
 
 headers = {
     'Accept': '*/*',
     'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
     'Host': 'apps.eias.fas.gov.ru',
     'Origin': 'http://apps.eias.fas.gov.ru',
     'Referer': 'http://apps.eias.fas.gov.ru/FindCem/',
@@ -39,13 +53,9 @@ headers = {
     'Connection': 'keep-alive',
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest',
 }
 
-
-
-
-# resp = session.post(url=url, data=payload, cookies=cookie, headers=headers)
-resp = session.post(url=test_url, headers=headers, data=payload, cookies=cookie)
-# print(resp.text)
-
-
+"""Получаем таблицу с данными"""
+resp = session.post(url=url, headers=headers, data=payload)
+print(resp.text)
